af53312e4c2bd9e388dd93918f7635740dabdc0d
zengzh <mrzengzzzz@gmail.com>
2023-03-12 15:23:14 +0000

    add fileheader

--- Acwing/chapter_1/quick_sort.cpp
+++ Acwing/chapter_1/quick_sort.cpp
@@ -1,16 +1,31 @@
+/*
+ * @Author: zengzh 
+ * @Date: 2023-03-12 15:22:53 
+ * @Last Modified by:   zengzh 
+ * @Last Modified time: 2023-03-12 15:22:53 
+ */
+
+#include <stdio.h>
 #include <algorithm>
 using namespace std;
 
 void quick_sort(int q[], int l, int r)
 {
-    if (l >= r) return;
-    int i = l -1, j =r +1, x = q[(l+r)>>1];
-    while (i < j){
-        do i ++; while (q[i] < x);
-        do j --; while (q[j] > x);
-        if (i < j) swap(q[i],q[j]);
+    if (l >= r)
+        return;
+    int i = l - 1, j = r + 1, x = q[(l + r) >> 1];
+    while (i < j)
+    {
+        do
+            i++;
+        while (q[i] < x);
+        do
+            j--;
+        while (q[j] > x);
+        if (i < j)
+            swap(q[i], q[j]);
+        quick_sort(q, l, j), quick_sort(q, j + 1, r);
     }
-    quick_sort(q, l, j), quick_sort(q, j+1, r);
 }
 
 int main()
