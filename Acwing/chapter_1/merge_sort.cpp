50cf0724f2933b6d03359e2f5853dc36831f915f
zengzh <mrzengzzzz@gmail.com>
2023-03-14 15:33:46 +0000

    check sort

--- Acwing/chapter_1/merge_sort.cpp
+++ Acwing/chapter_1/merge_sort.cpp
@@ -21,10 +21,10 @@ void merge_sort(int q[], int l, int r)
         return;
     int mid = (l + r) >> 1;
     merge_sort(q, l, mid), merge_sort(q, mid + 1, r);
-    int i = l, j = mid + 1, k = 0;
+    int k = 0, i = l, j = mid + 1;
     while (i <= mid && j <= r)
     {
-        if (q[i] <= q[j])
+        if (q[i] < q[j])
             tmp[k++] = q[i++];
         else
             tmp[k++] = q[j++];
@@ -33,10 +33,9 @@ void merge_sort(int q[], int l, int r)
         tmp[k++] = q[i++];
     while (j <= r)
         tmp[k++] = q[j++];
-    for (i = l, j = 0; i <= r; i++, j++)
+    for (int i = l, j = 0; i <= r; i++, j++)
         q[i] = tmp[j];
 }
-
 int main()
 {
     int q[] = {1, 3, 2, 4, 5, 6, 7, 8, 9, 10};
