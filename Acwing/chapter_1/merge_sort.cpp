b00a8ee526725b8539260fef2117d9c11535cd87
zengzh <mrzengzzzz@gmail.com>
2023-03-24 14:47:30 +0000

    check qucik sort

--- Acwing/chapter_1/merge_sort.cpp
+++ Acwing/chapter_1/merge_sort.cpp
@@ -25,16 +25,22 @@ void merge_sort(int q[], int l, int r)
     while (i <= mid && j <= r)
     {
         if (q[i] < q[j])
+        {
             tmp[k++] = q[i++];
+        }
         else
+        {
             tmp[k++] = q[j++];
+        }
     }
     while (i <= mid)
         tmp[k++] = q[i++];
     while (j <= r)
         tmp[k++] = q[j++];
     for (int i = l, j = 0; i <= r; i++, j++)
+    {
         q[i] = tmp[j];
+    }
 }
 int main()
 {
