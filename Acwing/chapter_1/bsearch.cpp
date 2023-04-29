6fc6511a250b1bbe9334daeab7a9ecb272fe5b21
zengzh <mrzengzzzz@gmail.com>
2023-04-29 12:48:06 +0000

    add bsearch func

--- Acwing/chapter_1/bsearch.cpp
+++ Acwing/chapter_1/bsearch.cpp
@@ -5,11 +5,29 @@
  * @Last Modified time: 2023-04-29 09:15:41
  */
 
-// 1 有序
-// 2 check mid
-// 3 l = mid; r = mid - 1; mid = l + r +1 >> 1
-// 4 r = mid; l = mid + 1; mid = l + r >> 1
-
+// 1 有序 bool check(int x) {}
+// int bsearch_1(int l, int r)
+// {
+//     while (l < r)
+//     {
+//         int mid = l + r >> 1;
+//         if check (mid)
+//             r = mid;
+//         l = mid + 1;
+//     }
+//     return l;
+// }
+// int bsearch_2(int l, int r)
+// {
+//     while (l < r)
+//     {
+//         int mid = l + r + 1 >> 1;
+//         if check (mid)
+//             l = mid;
+//         r = mid - 1;
+//     }
+//     return l;
+// }
 
 #include <stdio.h>
 #include <algorithm>
