d7b2a412ffc082f63aa15c1cc42b5336996709df
zengzh <mrzengzzzz@gmail.com>
2023-12-09 12:08:32 +0000

    check qucik sort

--- Acwing/chapter_1/quick_sort.cpp
+++ Acwing/chapter_1/quick_sort.cpp
@@ -15,20 +15,25 @@ using namespace std;
 
 void quick_sort(int q[], int l, int r)
 {
-    if ( l >= r) return;
-    int i = l -1 , j = i + 1, mid = q[(l + r) >> 1];
-    while ( i < j)
+    if (l >= r)
+        return;
+    int i = l - 1, j = r + 1, mid = q[(l + r) >> 1];
+    while (i < j)
     {
-        do i++;
-        while(q[i] < mid);
-        do j--;
-        while(q[j]> mid);
-        if (i <j){
+        do
+            i++;
+        while (q[i] < mid);
+        do
+            j--;
+        while (q[j] > mid);
+        if (i < j)
+        {
             swap(q[i], q[j]);
         }
     }
-    quick_sort(q, l, j), quick_sort(q, j+1, r);
+    quick_sort(q, l, j), quick_sort(q, j + 1, r);
 }
+
 
 int main()
 {
